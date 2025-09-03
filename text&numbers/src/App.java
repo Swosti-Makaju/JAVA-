45b561b8ee2b4817e50f616756884d89736972ee
Swosti Makaju <swosti.makaju@gmail.com>
2025-09-03 17:59:04 +0545

    java

---
+++ text&numbers/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("This is a sample Java application.");
+        System.out.println("It prints multiple lines to the console.");
+        System.out.println(111111111);
+        System.out.println(222222222);
+        System.out.println(333333333);
+    }
+}
+
+//  System.out.println(Hello, World!); will throw an error because Hello, World! is not in quotes.
+//  Number literals like 111111111 are valid in Java and will print correctly. Numbers can be printed directly without quotes.
