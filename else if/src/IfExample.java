7fecd6553d1daa05b5cff5b6b2e71264f6134c2f
Swosti Makaju <swosti.makaju@gmail.com>
2025-09-13 20:30:04 +0545

    if use in java

---
+++ else if/src/IfExample.java
@@ -0,0 +1,13 @@
+/*
+ * if statement example
+ */
+
+public class IfExample {
+    public static void main(String[] args) {
+        int age = 18;
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        }
+    }
+}
