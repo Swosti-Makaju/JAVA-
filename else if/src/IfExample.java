bb22c37028bcf1cf3cd91e90e13516aa0c8097d8
Swosti Makaju <swosti.makaju@gmail.com>
2025-09-14 20:34:34 +0545

    added code

--- else if/src/IfExample.java
+++ else if/src/IfExample.java
@@ -7,7 +7,14 @@ public class IfExample {
         int age = 18;
 
         if (age >= 18) {
-            System.out.println("You are eligible to vote.");
+            System.out.println("You are eligible to vote."); //you can change the value of age to test the condition . output will change accordingly
+        }
+        int number = 10; 
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number."); // This block will execute because the condition is true
+        }
+        if(number>0){
+            System.out.println(number + " is a positive number."); // This block will execute because the condition is true
         }
     }
 }
