b39b500291881fb6957987880896c14313045160
Swosti Makaju <swosti.makaju@gmail.com>
2025-09-14 20:42:21 +0545

    added code

--- else if/src/ElseIfExample.java
+++ else if/src/ElseIfExample.java
@@ -11,5 +11,45 @@ public class ElseIfExample {
         } else {
             System.out.println("Grade: Fail");
         }
+
+        int number = 0;
+        if (number > 0) {
+            System.out.println(number + " is a positive number.");
+        } else if (number < 0) {
+            System.out.println(number + " is a negative number.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        int day = 3;
+        if (day == 1) { 
+            System.out.println("Monday");
+        } else if (day == 2) {
+            System.out.println("Tuesday");
+        } else if (day == 3) {
+            System.out.println("Wednesday");
+        } else if (day == 4) {
+            System.out.println("Thursday");
+        } else if (day == 5) {
+            System.out.println("Friday");
+        } else if (day == 6) {
+            System.out.println("Saturday");
+        } else if (day == 7) {
+            System.out.println("Sunday");
+        } else {
+            System.out.println("Invalid day number.");
+        }
+
+        int age = 20;
+        boolean hasVoterID = true;  
+        if (age >= 18) {
+            if (hasVoterID) {
+                System.out.println("You can vote.");
+            } else {
+                System.out.println("You need a voter ID to vote.");
+            }
+        } else {
+            System.out.println("You are too young to vote.");
+        }
     }
 }
