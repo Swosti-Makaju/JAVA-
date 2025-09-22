f5ec5ab67030ddebdaa5fe07427c55d542a587b7
Swosti Makaju <swosti.makaju@gmail.com>
2025-09-22 19:23:11 +0545

    code

---
+++ methods/src/method.java
@@ -0,0 +1,16 @@
+/*
+ * method in java is a collection of statements that are grouped together to perform an operation.
+ * method is also known as function.
+ */
+
+public class method {
+
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+
+    public static void main(String[] args) {
+        myMethod(); //output: I just got executed!
+    }
+
+}
