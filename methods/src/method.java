def15fb36c9971edc6463c3f1c79469f4047a58f
Swosti Makaju <swosti.makaju@gmail.com>
2025-09-22 19:27:54 +0545

    code

--- methods/src/method.java
+++ methods/src/method.java
@@ -11,6 +11,24 @@ public class method {
 
     public static void main(String[] args) {
         myMethod(); //output: I just got executed!
+
     }
 
 }
+/*
+ * public class Main {
+  static void myMethod() {
+    System.out.println("I just got executed!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+    myMethod();
+    myMethod();
+  }
+}
+
+// I just got executed!
+// I just got executed!
+// I just got executed!
+ */
