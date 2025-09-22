acd45d202e1af8e00ab8e884bfbef930eb66a16e
Swosti Makaju <swosti.makaju@gmail.com>
2025-09-22 19:33:54 +0545

    code

--- methods/src/parameterargument.java
+++ methods/src/parameterargument.java
@@ -9,4 +9,13 @@ public class parameterargument {
         myMethod("Anja"); //output: Anja Nielsen
         
     }
+    //multiple parameters
+    static void myMethod2(String fname, int age) {
+        System.out.println(fname + " is " + age);
+    }
+    public static void main2(String[] args) {
+        myMethod2("Liam", 5); //output: Liam is 5
+        myMethod2("Jenny", 8); //output: Jenny is 8
+        myMethod2("Anja", 31); //output: Anja is 31
+    }
 }
