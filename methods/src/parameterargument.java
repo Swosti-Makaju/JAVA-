1ca8d36dfefd0c6f20a514c88ae70d07c873030b
Swosti Makaju <swosti.makaju@gmail.com>
2025-09-22 19:36:20 +0545

    code

--- methods/src/parameterargument.java
+++ methods/src/parameterargument.java
@@ -4,28 +4,46 @@ public class parameterargument {
     }
 
     public static void main(String[] args) {
-        myMethod("Henry"); //output: Henry Nielsen 
-        myMethod("Jenny"); //output: Jenny Nielsen
-        myMethod("Anja"); //output: Anja Nielsen
-        
+        myMethod("Henry"); // output: Henry Nielsen
+        myMethod("Jenny"); // output: Jenny Nielsen
+        myMethod("Anja"); // output: Anja Nielsen
+
     }
-    //multiple parameters
+
+    // Create a checkAge() method with an integer variable called age
+    static void checkAge(int age) {
+        if (age < 18) {
+            System.out.println("Access denied - You are not old enough!");
+        } else {
+            System.out.println("Access granted - You are old enough!");
+        }
+    }
+
+    public static void main1(String[] args) {
+        checkAge(20); // output: Access granted - You are old enough!
+        checkAge(17); // output: Access denied - You are not old enough!
+        checkAge(18); // output: Access granted - You are old enough!
+    }
+
+    // multiple parameters
     static void myMethod2(String fname, int age) {
         System.out.println(fname + " is " + age);
     }
+
     public static void main2(String[] args) {
-        myMethod2("Liam", 5); //output: Liam is 5
-        myMethod2("Jenny", 8); //output: Jenny is 8
-        myMethod2("Anja", 31); //output: Anja is 31
+        myMethod2("Liam", 5); // output: Liam is 5
+        myMethod2("Jenny", 8); // output: Jenny is 8
+        myMethod2("Anja", 31); // output: Anja is 31
     }
 
-    //return value
+    // return value
     static int myMethod3(int x) {
         return 5 + x;
     }
+
     public static void main3(String[] args) {
-        System.out.println(myMethod3(3)); //output: 8
-        System.out.println(myMethod3(5)); //output: 10
-        System.out.println(myMethod3(7)); //output: 12
+        System.out.println(myMethod3(3)); // output: 8
+        System.out.println(myMethod3(5)); // output: 10
+        System.out.println(myMethod3(7)); // output: 12
     }
 }
