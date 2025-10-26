1a1ca3feabe473fa681ccf7ea991c16abfb9b842
Swosti Makaju <swosti.makaju@gmail.com>
2025-10-26 19:13:26 +0545

    recursion

--- methods/src/recursion.java
+++ methods/src/recursion.java
@@ -11,6 +11,24 @@ public class recursion {
             return n * factorial(n - 1); // Recursive case
         }
     }
+ // Example of a simple recursive method to count down from a number
+    public static int countdown(int n) {
+        if (n <= 0) {
+            System.out.println("Done!");
+            return n;
+        } else {
+            System.out.println(n);
+            return countdown(n - 1);
+        }
+    }   
 
+// Example of a recursive method to compute the nth Fibonacci number
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n; // Base case: return n if it's 0 or 1
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2); // Recursive case
+        }
+    }
     
 }
